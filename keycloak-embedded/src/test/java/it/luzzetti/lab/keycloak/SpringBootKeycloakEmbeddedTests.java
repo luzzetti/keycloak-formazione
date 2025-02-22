e5d831877fd088b07dd3b396ac02533182f7c475
Christian Luzzetti <c.luzzetti@innovationengineering.eu>
2025-02-22 22:02:03 +0100

    Mi ero scordato mezza roba

---
+++ keycloak-embedded/src/test/java/it/luzzetti/lab/keycloak/SpringBootKeycloakEmbeddedTests.java
@@ -0,0 +1,13 @@
+package it.luzzetti.lab.keycloak;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootKeycloakEmbeddedTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
